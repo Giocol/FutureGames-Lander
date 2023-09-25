372439210fa4991cd071fd1f11702684a2353024
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2023-09-25 11:45:13 +0200

    feat: basic landing zone implementation with debug message

---
+++ Assets/Scripts/Environment/LandingZone.cs
@@ -0,0 +1,22 @@
+﻿using Ship;
+using System;
+using UnityEngine;
+
+namespace Environment {
+    public class LandingZone : MonoBehaviour{
+
+        private void OnCollisionEnter(Collision other) {
+            //TODO: get the right component, the one that handles touchdowns and crashes, not ShipPhysics
+            var shipComputer = other.gameObject.GetComponent<ShipPhysics>();
+            if(shipComputer) {
+                //TODO: call shipComputer.OnLanding instead
+                OnLanding();
+            }
+        }
+
+        //TODO: delete this and call shipComputer.OnLanding();
+        private void OnLanding() {
+            Debug.Log("LANDED!!!!!");
+        }
+    }
+}
