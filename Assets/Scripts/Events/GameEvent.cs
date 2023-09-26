b5a12a3f8ab346b9eab49382fbd6ae8c311d2f90
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2023-09-26 14:35:32 +0200

    tech: implement event prototype

---
+++ Assets/Scripts/Events/GameEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events {
+    //Took heavy inspiration from Ryan Hipple's Scriptable Objects talk @Unite2017 (https://www.youtube.com/watch?v=raQ3iHhE_Kk)
+
+    [CreateAssetMenu(menuName = "Scriptable Objects/Game Event")]
+    public class GameEvent : ScriptableObject {
+        private List<GameEventListener> listeners = new List<GameEventListener>();
+
+        public void Raise() {
+            for(int i = listeners.Count - 1; i >= 0; i--) {
+                listeners[i].OnEventRaised();
+            }
+        }
+
+        public void RegisterListener(GameEventListener listener) {
+            listeners.Add(listener);
+        }
+
+        public void UnregisterListener(GameEventListener listener) {
+            listeners.Remove(listener);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Events/GameEventListener.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+namespace Events {
+    //Took heavy inspiration from Ryan Hipple's Scriptable Objects talk @Unite2017 (https://www.youtube.com/watch?v=raQ3iHhE_Kk)
+    
+    public class GameEventListener : MonoBehaviour {
+        public GameEvent gameEvent;
+        public UnityEvent response;
+
+        private void OnEnable() {
+            gameEvent.RegisterListener(this);
+        }
+
+        private void OnDisable() {
+            gameEvent.UnregisterListener(this);
+        }
+
+        public void OnEventRaised() {
+            response.Invoke();
+        }
+    }
+}
