a386020a601baa92f3959e51f0d3facf40f23434
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2023-10-04 15:54:56 +0200

    feat: initial growing turret camera implementation

--- Assets/Scripts/Ship/ThreatCameraController.cs
+++ Assets/Scripts/Ship/ThreatCameraController.cs
@@ -6,7 +6,7 @@ using Utils;
 namespace Ship {
     public class ThreatCameraController : MonoBehaviour {
         [SerializeField] private RawImage turretRenderTextureImage;
-
+        [SerializeField] private float dangerCameraGrowingSpeed = 0.01f;
         private GameObject currentTarget;
         private GameObject[] turrets;
         private bool hasTarget;
@@ -17,11 +17,20 @@ namespace Ship {
         }
 
         private void Update() {
-            if(!hasTarget) {
+            if(!hasTarget && turrets.Length > 0) {
                 currentTarget = SortingUtils.GetClosestTarget(transform.position, turrets);
                 hasTarget = true;
             }
+            //TODO: final position, need to smoothly do the transition
+            //turretRenderTextureImage.rectTransform.anchoredPosition = Vector3.one;
+            //turretRenderTextureImage.rectTransform.sizeDelta = Vector2.one;
+
+            turretRenderTextureImage.rectTransform.anchoredPosition = new Vector2(Mathf.Lerp(turretRenderTextureImage.rectTransform.anchoredPosition.x, 1f, dangerCameraGrowingSpeed), Mathf.Lerp(turretRenderTextureImage.rectTransform.anchoredPosition.y, 1f, dangerCameraGrowingSpeed));
+            turretRenderTextureImage.rectTransform.sizeDelta = new Vector2(Mathf.Lerp(turretRenderTextureImage.rectTransform.sizeDelta.x, 1f, dangerCameraGrowingSpeed), Mathf.Lerp(turretRenderTextureImage.rectTransform.sizeDelta.y, 1f, dangerCameraGrowingSpeed));
+
         }
+
+
 
     }
 }
