bedb49e9c604d4033dbb512e3eb79f817ffdf9a8
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2023-10-09 11:21:11 +0200

    chore: add missing namespace and minor prefab tweaks

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -1,15 +1,15 @@
 using Ship;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthUI : MonoBehaviour {
-    [SerializeField] private ShipHull hull;
-    [SerializeField] private Image hullHealthFill;
+namespace UI
+{
+    public class HealthUI : MonoBehaviour {
+        [SerializeField] private ShipHull hull;
+        [SerializeField] private Image hullHealthFill;
 
-    private void Update() {
-        hullHealthFill.fillAmount = 1 - (float) hull.HullHealth / hull.MaxHullHealth;
+        private void Update() {
+            hullHealthFill.fillAmount = 1 - (float) hull.HullHealth / hull.MaxHullHealth;
+        }
     }
 }
