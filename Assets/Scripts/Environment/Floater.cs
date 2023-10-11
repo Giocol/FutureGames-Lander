6f6cbf49e480e0f6855f1855ac550634eef8f9b6
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2023-10-11 09:53:41 +0200

    feat: implement floating for pickups

---
+++ Assets/Scripts/Environment/Floater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Environment {
+    public class Floater : MonoBehaviour{
+        [SerializeField] private float floatingSpeed = 5f;
+        [SerializeField] private float floatingAmplitude = 1f;
+        private float floatingCenter;
+        private Vector3 currentPosition;
+
+        private void Start() {
+            floatingCenter = transform.position.y;
+        }
+
+        private void Update() {
+            currentPosition = transform.position;
+            transform.position = new Vector3(currentPosition.x, Mathf.Sin(Time.time * floatingSpeed) * floatingAmplitude + floatingCenter, currentPosition.z);
+        }
+    }
+}
